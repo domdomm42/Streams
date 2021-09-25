194c8b31ff591dd2a3b453ff8012acc451d5a0fe
Ridho Yusuf <ridho.yusuf@student.unsw.edu.au>
2021-09-25 18:53:28 +1000

    finished auth_register_v1

--- src/auth.py
+++ src/auth.py
@@ -10,23 +10,35 @@ def auth_login_v1(email, password):
 
 def auth_register_v1(email, password, name_first, name_last):
 
-    # Check all inputs
+    store = data_store.get()
+
+    # Check all inputs and store in data_store
     check_email(email)
+    store['emails'].append(email)
+
     check_password(password)
+    store['passwords'].append(password)
+
     check_first_name(name_first)
+    store['first_names'].append(name_first)
+
     check_last_name(name_last)
-
-    # Create user_handle 
-
-    return {
-        'auth_user_id': 1,
-    }
+    store['last_names'].append(name_last)
+    
+    # Create user_handle and store in data_store, return auth_user_id
+    # NOTE: auth_user_id is the index of the user in the lists
+    auth_user_ID = create_user_handle(name_first, name_last)
+    
+    return auth_user_ID
 
 
 def check_email(email):
+
+    store = data_store.get()
+
     regex = r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$'
 
-    if re.fullmatch(regex, email):
+    if re.fullmatch(regex, email) and email not in store['emails']:
         pass
     else:
         raise InputError('Invalid email!')
@@ -54,7 +66,7 @@ def create_user_handle(name_first, name_last):
     user_handle = (name_first.lower() + name_last.lower())[0:19]
 
     # Obtain data
-    data = data_store.get()
+    store = data_store.get()
     
     # Check if the user_handle is unique,
     # If user_handle is not unique, then the function will add
@@ -62,21 +74,26 @@ def create_user_handle(name_first, name_last):
     user_handle_copy = user_handle
 
     i = 1
-    while user_handle in data['user_handles']:
+    while user_handle in store['user_handles']:
         user_handle = user_handle_copy + str(i)
         i += 1
     
-    data['user_handles'].append(user_handle)
-    # print(data['user_handles'])
-    return user_handle
-
+    store['user_handles'].append(user_handle)
+    auth_user_ID = store['user_handles'].index(user_handle)
+    print(store)
+    return auth_user_ID
 
 # Debugging + Testing purposes
 if __name__ == '__main__':
 
-    # Testing if create_user_handle works properly 
-    create_user_handle('Joe', 'Jimsfkjhydsyfdysfdyhs')
-    create_user_handle('Joe', 'Jimsfkjhydsyfdysfdyhs')
-    create_user_handle('Joe', 'Jimsfkjhydsyfdysfdyhs')
-    create_user_handle('Joe', 'Jimsfkjhydsyfdysfdyhs')
+    
+    auth_register_v1('joe123@gmail.com', 'password', 'Joe', 'Smith')
+    auth_register_v1('joe1233@gmail.com', 'password', 'Joe', 'Smith')
+
+
+    # Testing create_user_handle works correctly for people with the same name
+    # create_user_handle('Joe', 'Jimsfkjhydsyfdysfdyhs')
+    # create_user_handle('Joe', 'Jimsfkjhydsyfdysfdyhs')
+    # create_user_handle('Joe', 'Jimsfkjhydsyfdysfdyhs')
+    # create_user_handle('Joe', 'Jimsfkjhydsyfdysfdyhs')
     
--- src/data_store.py
+++ src/data_store.py
@@ -26,7 +26,6 @@ Example usage:
 
 ## YOU SHOULD MODIFY THIS OBJECT BELOW
 initial_object = {
-    'users': [],
     'emails': [],
     'passwords': [],
     'first_names': [],
