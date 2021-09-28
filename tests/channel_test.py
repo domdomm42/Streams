66d07a343504daa7a785f6e348f6b8b22d60f110
Li Oudom <z5329509@ad.unsw.edu.au>
2021-09-28 14:55:06 +1000

    Added tests for channels_create function

---
+++ tests/channel_test.py
@@ -0,0 +1,78 @@
+import pytest
+
+from src.other import clear_v1
+from src.error import InputError
+from src.auth import auth_login_v1, auth_register_v1
+from src.channels import channels_list_v1, channels_listall_v1, channels_create_v1
+from src.channel import channel_join_v1
+
+''' 
+Channels testing documentation
+Setup creates the background for each channel list test by registering,
+logging, and creating a channel for each user. A private channel for 
+user Marry (channel 1) and a public channel for user Joe (channel 0 and 1).  
+'''
+
+@pytest.fixture
+def setup():
+    clear_v1()
+    register_userID = auth_register_v1('joe123@gmail.com', 'password', 'Joe', 'Smith')
+    login_joe = auth_login_v1('joe123@gmail.com', 'password')
+    channel_joe = channels_create_v1(login_joe, 'Joe', False)
+    register_userID = auth_register_v1('marryjoe222@gmail.com', 'passwordM', 'Marry', 'Joe')
+    login_marry = auth_login_v1('marryjoe222@gmail.com', 'passwordM')
+    channel_marry = channels_create_v1(login_marry, 'Marry', True)
+    channel_join_v1(login_joe, channel_marry)
+    return login_marry
+
+# Simple Listing tests to return all channels created
+def test_all_channels(setup):
+    assert channels_list_v1(setup) == {
+        'channels': [
+        	{
+        		'channel_id': 0,
+        		'name': 'Joe',
+        	},
+            {
+                'channel_id': 1,
+                'name': 'Marry'
+            }
+        ],
+    }
+
+# Simple listing tests to return all channels that the given id is a part of
+def test_given_channels(setup):
+     assert channels_list_v1(setup) == {
+        'channels': [
+        	{
+        		'channel_id': 1,
+        		'name': 'Marry',
+        	}
+        ],
+     }     
+
+# Testing Invalid names for channel creation
+def test_empty_name():
+    with pytest.raises(InputError):
+        channels_create_v1(1, '', True)
+
+def test_long_name():
+    with pytest.raises(InputError):
+        channels_create_v1(1, 'bigbigbigbigbigbigbigbig', True)
+
+def test_empty_name():
+    with pytest.raises(InputError):
+        channels_create_v1(1, '', False)
+
+def test_long_name():
+    with pytest.raises(InputError):
+        channels_create_v1(1, 'bigbigbigbigbigbigbigbig', False)
+
+# Testing simple channel creation
+def test_new_channel():
+    clear_v1()
+    assert channels_create_v1(0, 'Joe', True) == 0
+    assert channels_create_v1(1, 'Marry', True) == 1
+    assert channels_create_v1(0, 'Joeseph', True) == 2
+    
+        
