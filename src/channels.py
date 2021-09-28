a6a2fdbf8797fb7160e2144399eb33f315b14f64
Li Oudom <z5329509@ad.unsw.edu.au>
2021-09-28 14:57:12 +1000

    Fixed channels_create_v1 indexing, user_id starts at 0 instead of 1 now

--- src/channels.py
+++ src/channels.py
@@ -1,3 +1,6 @@
+from src.data_store import data_store
+from src.error import InputError
+
 def channels_list_v1(auth_user_id):
     return {
         'channels': [
@@ -19,6 +22,36 @@ def channels_listall_v1(auth_user_id):
     }
 
 def channels_create_v1(auth_user_id, name, is_public):
-    return {
-        'channel_id': 1,
-    }
+
+    # Check length of name
+    check_channel_name(name)
+
+    # Stores necessary data into the data store 
+    store = data_store.get()
+
+    store['channels']['owner_user_id'].append(auth_user_id)
+    store['channels']['channel_name'].append(name)
+    store['channels']['is_public'].append(is_public)
+
+    data_store.set(store)
+
+    # Loops through the channel owner_user_id with a seperate 
+    # iterator to find the channel_id(index)
+    i = 0
+    for x in store["channels"]["owner_user_id"]:
+        i += 1
+    
+    # We want to start index with 0, the for loop above
+    # Gives index that is 1 over what we expect hence minus 1.
+    channel_id = i - 1
+
+    return channel_id 
+
+
+    # Function to check if name is within 1 and 20 characters.
+def check_channel_name(name):
+    if len(name) >= 1 and len(name) <= 20:
+        pass
+    else:
+        raise InputError('Length of channel name must be between 1 and 20 characters!')
+        
