dbf46ea1c98458252eed46bb39c5d953f0addac9
Z5275171 <Z5275171@ad.unsw.edu.au>
2021-09-28 18:00:27 +1000

    fixed message

--- src/channel.py
+++ src/channel.py
@@ -1,6 +1,29 @@
 def channel_invite_v1(auth_user_id, channel_id, u_id):
+
+    # Check
+    check_invalid_channel_id(channel_id)
+    check_invalid_u_id(u_id)
+    check_member_u_id(u_id)
+
+    check_autorised_id(u_id)
+
+    # Store
+    store = data_store.get()
+
+    store['channels']['owner_user_id'].append(auth_user_id)
+    store['channels']['channel_id'].append(channel_id)
+    store['channels']['u_id'].append(u_id)
+
+    data_store.set(store)
     return {
+
     }
+
+
+# store['channels']['channel_id']['name'],
+#
+#
+# all_members.append(auth_user_id)
 
 def channel_details_v1(auth_user_id, channel_id):
     return {
@@ -26,19 +49,72 @@ def channel_details_v1(auth_user_id, channel_id):
     }
 
 def channel_messages_v1(auth_user_id, channel_id, start):
+
+    # Check
+    check_invalid_channel_id(channel_id)
+    check_invalid_start(channel_id, start)
+
+    check_autorised_id(u_id)
+
+    # Loop
+    messages_list = []
+    place = start
+    for message in intial_object['channels']['messages'][0]:
+        messages_list.append(message)
+        place += 1
+        if place == 50:
+            break
+    if place < 50:
+        place = -1
+
     return {
-        'messages': [
-            {
-                'message_id': 1,
-                'u_id': 1,
-                'message': 'Hello world',
-                'time_created': 1582426789,
-            }
-        ],
-        'start': 0,
-        'end': 50,
+        'message': message,
+        'start': start,
+        'end': place
     }
 
 def channel_join_v1(auth_user_id, channel_id):
     return {
     }
+
+
+# InputError
+# Check invalid channel_id
+def check_invalid_channel_id(channel_id):
+    if store['channels'].has_key(channel_id) == True:
+        pass
+    else:
+        raise InputError('Invalid channel id')
+
+
+# Check invalid u_id
+def check_invalid_u_id(u_id):
+    if store['channels'].has_key(u_id) == True:
+        pass
+    else:
+        raise InputError('Invalid u id')
+
+
+# Check member u_id
+def check_member_u_id(u_id):
+    if u_id in channels.values() == True:
+        pass
+    else:
+        raise InputError('Permission dinined!')
+
+# Check start
+def check_invalid_start(channel_id, start):
+    if start <= len(['channel_id']['messages']):
+        pass
+    else:
+        raise InputError('Permission dinined!')
+
+# AccessError
+# Check authorised
+def check_autorised_id(u_id):
+    if u_id in channels.values() == True):
+        pass
+    else:
+        raise AccessError('Permission dinined!')
+
+# (store['channels'].has_key(channel_id) == True) and (u_id in channels.values() == True):
