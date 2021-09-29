6b4de6c4e91a76f04c85cc241dddd14ad04dc2ee
Z5275171 <Z5275171@ad.unsw.edu.au>
2021-09-29 15:16:16 +1000

    change the head import

--- src/data_store.py
+++ src/data_store.py
@@ -41,6 +41,7 @@ initial_object = {
     # the 0th index of all_members is a list of strings/numbers containing
     # all the members
     'channels': {
+        'name': [],
         'owner_user_id': [],
         'is_public': [],
         'all_members': [],
