23e3566b98e229e5bea759d6cc2c6b47cc0fe8be
Siyuan Tang <z5212799@ad.unsw.edu.au>
2021-09-29 18:45:59 +1000

    Update Correct output for detail function

--- src/channel.py
+++ src/channel.py
@@ -4,22 +4,60 @@ from src.error import InputError, AccessError
 
 
 
+
+
 def channel_invite_v1(auth_user_id, channel_id, u_id):
     return {
     }
 
 def channel_details_v1(auth_user_id, channel_id):
     store = data_store.get()
-
+    
     #check input
     check_channel_id(channel_id)
+    
+
     check_authority(auth_user_id, channel_id)
+    
     name = store['channels']['channel_name'][channel_id]
     public = store['channels']['is_public'][channel_id]
     owners = store['channels']['owner_user_id'][channel_id]
-    members = store['channels']['all_members'][channel_id]
+    print(owners)
     
-    return {name, public, owners, members}
+    members = store['channels']['all_members'][channel_id]
+    print(members)
+    owner_details = []
+    
+    
+    user_email = store['users']['emails'][owners]
+    user_first_name = store['users']['first_names'][owners]
+    user_last_name = store['users']['last_names'][owners]
+    user_handles = store['users']['user_handles'][owners]
+    owner_details.append({'u_id' : owners, 'email' : user_email, 
+    'name_first' : user_first_name, 'name_last' : user_last_name, 'handle_str' : user_handles})
+
+    members_details = []
+
+
+    for member_users_id in members:
+        user_email = store['users']['emails'][member_users_id]
+        user_first_name = store['users']['first_names'][member_users_id]
+        user_last_name = store['users']['last_names'][member_users_id]
+        user_handles = store['users']['user_handles'][member_users_id]
+        members_details.append({'u_id' : member_users_id, 'email' : user_email, 
+        'name_first' : user_first_name, 'name_last' : user_last_name, 'handle_str' : user_handles})
+
+
+
+
+    return {
+        'name': name,
+        'is_public': public,
+        'owner_members': owner_details,
+        'all_members' : members_details
+
+        
+    }
     
         
     
@@ -54,19 +92,23 @@ def channel_join_v1(auth_user_id, channel_id):
 def check_channel_id(channel_id):
     store = data_store.get()
     i = 0
+    
     for element in store['channels']['channel_name']:
+        
         if i == channel_id:
-            pass
             return
+            
         i += 1
     raise InputError('Invalid input')
 
 def check_authority(auth_user_id, channel_id):
     store = data_store.get()
+   
+    
     for user in store['channels']['all_members'][channel_id]:
         if user == auth_user_id:
-            pass
             return
+            
     
     raise AccessError('Permission denied!')
             
@@ -74,7 +116,9 @@ def check_authority(auth_user_id, channel_id):
 #check user is a member or not
 def check_exist_member(auth_user_id, channel_id):
     store = data_store.get()
-    for user in store['channels']['all_members'][channel_id]:  
+    
+    for user in store['channels']['all_members'][channel_id]: 
+         
         if user == auth_user_id:
             raise InputError('You are a member already!')
         
@@ -90,3 +134,8 @@ def check_channel_status(channel_id, auth_user_id):
         
     
 
+
+
+
+
+    
