b6093d1c60cf753e6030fd23c27e0ce3e2b24837
Ridho Yusuf <ridho.yusuf@student.unsw.edu.au>
2021-10-01 14:47:07 +1000

    Update channel_test.py

--- tests/channel_test.py
+++ tests/channel_test.py
@@ -206,36 +206,3 @@ def test_valid_channel_id_join():
         ]
     }
     
-
-
-''' 
-Channels testing documentation
-Setup creates the background for each channel list test by registering,
-logging, and creating a channel for each user. A private channel for 
-user Marry (channel 1) and a public channel for user Joe (channel 0 and 1).  
-'''
-
-# Testing Invalid names for channel creation
-def test_empty_name():
-    with pytest.raises(InputError):
-        channels_create_v1(1, '', True)
-
-def test_long_name():
-    with pytest.raises(InputError):
-        channels_create_v1(1, 'bigbigbigbigbigbigbigbig', True)
-
-def test_empty_name():
-    with pytest.raises(InputError):
-        channels_create_v1(1, '', False)
-
-def test_long_name():
-    with pytest.raises(InputError):
-        channels_create_v1(1, 'bigbigbigbigbigbigbigbig', False)
-
-# Testing simple channel creation
-def test_new_channel():
-    clear_v1()
-    assert channels_create_v1(0, 'Joe', True) == 0
-    assert channels_create_v1(1, 'Marry', True) == 1
-    assert channels_create_v1(0, 'Joeseph', True) == 2
-    
