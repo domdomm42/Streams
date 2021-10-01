6f314042224a278fdedfc03fe244ccc6bd721153
Z5275171 <Z5275171@ad.unsw.edu.au>
2021-10-01 14:58:23 +1000

    fixed some style

--- src/channel.py
+++ src/channel.py
@@ -8,14 +8,12 @@ def channel_invite_v1(auth_user_id, channel_id, u_id):
     # Check
     check_invalid_channel_id(channel_id)
     check_invalid_u_id(u_id)
+
     check_member_u_id(channel_id, u_id)
 
     check_autorised_id(u_id)
 
     # Store
-
-    #store['channels']['owner_user_id'].append(auth_user_id)
-    #store['channels']['u_id'].append(u_id)
 
     store['channels']['all_members'][channel_id].append(u_id)
 
@@ -66,7 +64,7 @@ def channel_messages_v1(auth_user_id, channel_id, start):
     # Loop
     messages_list = []
     place = start
-    for message in intial_object['channels']['messages'][0]:
+    for message in initial_object['channels']['messages'][0]:
         messages_list.append(message)
         place += 1
         if place == 50:
@@ -97,7 +95,7 @@ def check_invalid_channel_id(channel_id):
             return
             
         i += 1
-    raise InputError('Invalid input')
+    raise InputError('Invalid channel id')
 
     
     
@@ -136,7 +134,7 @@ def check_invalid_start(channel_id, start):
 
 # AccessError
 # Check authorised
-def check_autorised_id(u_id):
+def check_autorised_id(u_id,auth_user_id,channel_id):
     store = data_store.get()
    
     
