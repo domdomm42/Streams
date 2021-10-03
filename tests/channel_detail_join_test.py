995abf8992d8745c609796d6c7506ae3d9c0bac7
Siyuan Tang <siyuan.tang@student.unsw.edu.au>
2021-10-03 15:17:05 +1100

    Update channel_detail_join_test.py

--- tests/channel_detail_join_test.py
+++ tests/channel_detail_join_test.py
@@ -2,7 +2,7 @@ import pytest
 from src.channel import channel_details_v1, channel_join_v1
 from src.error import InputError, AccessError
 from src.auth import auth_login_v1, auth_register_v1
-from src.channels import channels_list_v1, channels_listall_v1, channels_create_v1
+from src.channels import channels_create_v1
 from src.other import clear_v1
 
 #=====Input Error======================
