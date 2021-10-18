7f7e4c05b6ac045e6746f7b0ad6ec090e1347405
Augustus <Z5275171@ad.unsw.edu.au>
2021-10-18 18:27:33 +1100

    update users.py

--- src/users.py
+++ src/users.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from src.data_store import data_store
 
 # Users functions
@@ -123,4 +124,131 @@ def user_profile_sethandle_v1(toke, handle_str):
 
 
 
+=======
+from src.data_store import data_store
+
+# Users functions
+
+
+# List of all users
+def user_all_v1(token):
+
+
+    user_id = check_and_get_user_id(token)
+
+    store = data_store.get()
+
+    user = []
+
+    for x in store['users']['u_id']:
+
+        
+        user.append({'u_id': owners, 'email': user_email, 'name_first': user_first_name, 'name_last': user_last_name, 'handle_str': user_handles})
+
+
+
+    data_store.set(store)
+
+
+
+    return {user}
+
+
+# List of all valid users
+def user_profile_v1(token, u_id):
+
+    user_id = check_and_get_user_id(token)
+
+
+    store = data_store.get()
+
+    user = []
+
+    for x in store['users']['u_id']:
+        
+        if u_id < len(store['users']['user_handles']):
+            user.append({'u_id': owners, 'email': user_email, 'name_first': user_first_name, 'name_last': user_last_name, 'handle_str': user_handles})
+
+    
+    
+    data_store.set(store)
+
+    
+    
+    return {user}
+
+
+
+
+
+
+# Update name
+def user_profile_setname_v1(token, name_first, name_last):
+
+    user_id = check_and_get_user_id(token)
+
+    store = data_store.get()
+
+
+    new_name = {'name_first': name_first, 'name_last': name_last}
+
+    store['users']['u_id'].update(new_name)
+    
+    
+    data_store.set(store)
+
+
+
+    return {}
+
+
+
+
+# Update email
+def user_profile_setemail_v1(token, email):
+    
+    user_id = check_and_get_user_id(token)
+
+
+    store = data_store.get()
+    
+    
+    new_email = {'email': email}
+
+
+    store['users']['u_id'].update(new_email)
+    
+    
+    
+    
+    data_store.set(store)
+
+
+    return {}
+
+
+
+# Update handle
+def user_profile_sethandle_v1(toke, handle_str):
+
+    user_id = check_and_get_user_id(token)
+
+
+    store = data_store.get()
+
+
+
+    new_handle = {'handle_str': user_handles}
+
+
+    store['users']['u_id'].update(new_handle)
+
+
+    data_store.set(store)
+
+    return {}
+
+
+
+>>>>>>> update users functions and fixed token
 # Check Functions
