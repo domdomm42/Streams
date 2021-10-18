e2c113575d7b2a1808fb4cd6d39706983402e9b2
Augustus Liu <xuefeng.liu@student.unsw.edu.au>
2021-10-18 18:14:01 +1100

    Update users.py and use check_and_get_user_id() function

--- src/users.py
+++ src/users.py
@@ -1,17 +1,19 @@
-
+from src.data_store import data_store
 
 # Users functions
 
 
 # List of all users
-@APP.route("/users/all/v1", methods=['GET'])
 def user_all_v1(token):
+
+
+    user_id = check_and_get_user_id(token)
 
     store = data_store.get()
 
     user = []
 
-    for x in token:
+    for x in store['users']['u_id']:
 
         
         user.append({'u_id': owners, 'email': user_email, 'name_first': user_first_name, 'name_last': user_last_name, 'handle_str': user_handles})
@@ -22,18 +24,20 @@ def user_all_v1(token):
 
 
 
-    return dumps{user}
+    return {user}
 
 
 # List of all valid users
-@APP.route("/users/profile/v1", methods=['GET'])
 def user_profile_v1(token, u_id):
+
+    user_id = check_and_get_user_id(token)
+
 
     store = data_store.get()
 
     user = []
 
-    for x in ['channels']['all_members']
+    for x in store['users']['u_id']:
         
         if u_id < len(store['users']['user_handles']):
             user.append({'u_id': owners, 'email': user_email, 'name_first': user_first_name, 'name_last': user_last_name, 'handle_str': user_handles})
@@ -44,7 +48,7 @@ def user_profile_v1(token, u_id):
 
     
     
-    return dumps{user}
+    return {user}
 
 
 
@@ -52,15 +56,16 @@ def user_profile_v1(token, u_id):
 
 
 # Update name
-@APP.route("/users/profile/setname/v1", methods=['PUT'])
 def user_profile_setname_v1(token, name_first, name_last):
+
+    user_id = check_and_get_user_id(token)
 
     store = data_store.get()
 
 
     new_name = {'name_first': name_first, 'name_last': name_last}
 
-    token[users].update(new_name)
+    store['users']['u_id'].update(new_name)
     
     
     data_store.set(store)
@@ -73,16 +78,18 @@ def user_profile_setname_v1(token, name_first, name_last):
 
 
 # Update email
-@APP.route("/users/profile/setemail/v1", methods=['PUT'])
 def user_profile_setemail_v1(token, email):
     
+    user_id = check_and_get_user_id(token)
+
+
     store = data_store.get()
     
     
     new_email = {'email': email}
 
 
-    token[users].update(new_email)
+    store['users']['u_id'].update(new_email)
     
     
     
@@ -95,8 +102,10 @@ def user_profile_setemail_v1(token, email):
 
 
 # Update handle
-@APP.route("/users/profile/sethandle/v1", methods=['PUT'])
 def user_profile_sethandle_v1(toke, handle_str):
+
+    user_id = check_and_get_user_id(token)
+
 
     store = data_store.get()
 
@@ -105,21 +114,7 @@ def user_profile_sethandle_v1(toke, handle_str):
     new_handle = {'handle_str': user_handles}
 
 
-    token[users].update(new_handle)
-
-
-
-
-
-
-
-
-
-
-
-    # del {​'user_id': user_id, 'session_id': session_id}​ in store['logged_in_users']
-
-    # store['logged_in_users'].remove({​'user_id': user_id, 'session_id': session_id}​)
+    store['users']['u_id'].update(new_handle)
 
 
     data_store.set(store)
@@ -128,19 +123,4 @@ def user_profile_sethandle_v1(toke, handle_str):
 
 
 
-
-
-
-
-
-
-# IT2
-
-
-@APP.route("/channel/leave/v1", methods=['POST'])
-def channel_leave_v1(token, channel_id):
-    
-    del token['channels']['all_members'][channel_id]
-    
-    
-    return {}
+# Check Functions
