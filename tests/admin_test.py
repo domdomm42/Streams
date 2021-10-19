5104826ac1d8ce47c775b01ac7e5410be925439b
Li Oudom <z5329509@ad.unsw.edu.au>
2021-10-20 00:33:08 +1100

    New branch

---
+++ tests/admin_test.py
@@ -0,0 +1,20 @@
+import pytest
+import requests
+import jwt
+from src.auth import auth_login_v1, auth_register_v1, auth_logout_v1
+from src.error import InputError, AccessError
+from src.other import clear_v1
+from src.config import *
+from src.auth_auth_helpers import SECRET
+from src.data_store import *
+from src.admin import *
+
+BASE_URL = url
+
+# def test_simple_user_delete():
+#     auth_register_v1('domdomdom14@gmail.com', 'passwordDD', 'Oudom', 'Lim')
+#     auth_register_v1('domdomdommmm14@gmail.com', 'passwordD', 'Aasdasdw', 'Tim')
+#     auth_register_v1('dan214@gmail.com', 'passwordD', 'Dan', 'Guy')
+
+    
+
