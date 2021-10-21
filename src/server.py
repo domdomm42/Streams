1a6fca8c50beb2e5b621dc20a041001988868d95
Ridho Yusuf <ridho.yusuf@student.unsw.edu.au>
2021-10-21 16:22:40 +1100

    fix pylint issue

--- src/server.py
+++ src/server.py
@@ -10,7 +10,7 @@ from src.auth import auth_register_v1, auth_login_v1, auth_logout_v1
 from src.other import clear_v1
 from src.channels import channels_create_v1, channels_list_v1, channels_listall_v1
 from src.channel import channel_invite_v1, channel_join_v1, channel_details_v1, channel_leave_v1, channel_addowner_v1, channel_removeowner_v1, channel_messages_v1
-from src.message import message_send_v1, message_edit_v1, message_remove_v1
+from src.message import message_send_v1, message_edit_v1, message_remove_v1, message_senddm_v1
 from src.admin import admin_user_remove_v1, admin_userpermission_change_v1
 
 def quit_gracefully(*args):
