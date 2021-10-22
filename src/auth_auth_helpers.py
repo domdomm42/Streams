3648cfc1729a61ab26b6a5d7f4f4d507da29ba7b
Mohammad M <z5264362@ad.unsw.edu.au>
2021-10-23 03:39:00 +1100

    error checking and pylint scores

--- src/auth_auth_helpers.py
+++ src/auth_auth_helpers.py
@@ -37,7 +37,7 @@ def check_and_get_user_id(token):
         decoded_token = jwt.decode(token, SECRET, algorithms=['HS256'])
 
     except:
-        raise AccessError('Invalid token')
+        raise AccessError('Invalid token') from None
     user_id = decoded_token['user_id']
     session_id = decoded_token['session_id']
 
