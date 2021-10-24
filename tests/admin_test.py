bbcf424b595dee29f1ad80928ef30cb9658fd37d
Li Oudom <z5329509@ad.unsw.edu.au>
2021-10-24 22:12:31 +1100

    Added comments to the tests

--- tests/admin_test.py
+++ tests/admin_test.py
@@ -11,234 +11,179 @@ from src.auth_auth_helpers import SECRET
 
 BASE_URL = url
 
-def test_remove_admin():
+@pytest.fixture
+def setup():
     requests.delete(f'{BASE_URL}/clear/v1')
-    user_info_reg_1 = {"email": "marryjoe@gmail.com", "password": "password", "name_first": "Marry", "name_last": "Joe"}
-    user_info_reg_2 = {"email": "marryjane@gmail.com", "password": "passwordJ", "name_first": "Marry", "name_last": "Jane"}
 
-    response_data_1 = requests.post(f'{BASE_URL}/auth/register/v2', json = user_info_reg_1)
-    response_data_2 = requests.post(f'{BASE_URL}/auth/register/v2', json = user_info_reg_2)
+    #Create User Marry Joe
+    user_info = {'email': 'marryjoe123@gmail.com', 'password': 'password', 'name_first': 'Marry', 'name_last': 'Joe'}
+    marryjoe_data = requests.post(f'{BASE_URL}/auth/register/v2', json = user_info).json()
 
-    response_data_1 = response_data_1.json()
-    response_data_2 = response_data_2.json()
+    #Create User Marry Jane
+    user_info = {"email": "marryjane@gmail.com", "password": "passwordJ", "name_first": "Marry", "name_last": "Jane"}
+    marryjane_data = requests.post(f'{BASE_URL}/auth/register/v2', json = user_info).json()
 
-    kick_data = {'token': response_data_2['token'], 'u_id': response_data_1['auth_user_id']}
+    #Create user Marry
+    user_info = {"email": "marry@gmail.com", "password": "passwordz", "name_first": "Marry", "name_last": "Tom"}
+    marry_data = requests.post(f'{BASE_URL}/auth/register/v2', json = user_info).json()
+
+ 
+    return marryjoe_data, marryjane_data, marry_data
+
+
+def test_remove_admin(setup):
+    # Register marryjoe and marry jane
+    marryjoe_data, marryjane_data, _ = setup
+
+    kick_data = {'token': marryjane_data['token'], 'u_id': marryjoe_data['auth_user_id']}
     
+    # Remove marryjoe
     delete_data = requests.delete(f'{BASE_URL}/admin/user/remove/v1', json = kick_data)
     delete_data = delete_data.json()
-    #print(delete_data)
 
     assert delete_data['code'] == 403
 
 
 
-def test_change_permission():
-    requests.delete(f'{BASE_URL}/clear/v1')
-    user_info_reg_1 = {"email": "marryjoe@gmail.com", "password": "password", "name_first": "Marry", "name_last": "Joe"}
-    user_info_reg_2 = {"email": "marryjane@gmail.com", "password": "passwordJ", "name_first": "Marry", "name_last": "Jane"}
+def test_change_permission(setup):
+    marryjoe_data, marryjane_data, _ = setup
 
-    response_data_1 = requests.post(f'{BASE_URL}/auth/register/v2', json = user_info_reg_1)
-    response_data_2 = requests.post(f'{BASE_URL}/auth/register/v2', json = user_info_reg_2)
 
-    response_data_1 = response_data_1.json()
-    response_data_2 = response_data_2.json()
+    permission_change_data_1 = {'token': marryjoe_data['token'], 'u_id': marryjane_data['auth_user_id'], 'permission_id': 1}
+    permission_change_data_2 = {'token': marryjane_data['token'], 'u_id': marryjoe_data['auth_user_id'], 'permission_id': 2}
 
-    permission_change_data_1 = {'token': response_data_1['token'], 'u_id': response_data_2['auth_user_id'], 'permission_id': 1}
-    permission_change_data_2 = {'token': response_data_2['token'], 'u_id': response_data_1['auth_user_id'], 'permission_id': 2}
-
+    # Promote Marry jane
     requests.post(f'{BASE_URL}/admin/userpermission/change/v1', json = permission_change_data_1)
+    # Demote Marry Joe
     requests.post(f'{BASE_URL}/admin/userpermission/change/v1', json = permission_change_data_2)
 
-    kick_data = {'token': response_data_1['token'], 'u_id': response_data_2['auth_user_id']}
+    # Kick Marry Jane
+    kick_data = {'token': marryjoe_data['token'], 'u_id': marryjane_data['auth_user_id']}
     
     delete_data = requests.delete(f'{BASE_URL}/admin/user/remove/v1', json = kick_data)
     delete_data = delete_data.json()
-    #print(delete_data)
 
     assert delete_data['code'] == 403
 
-def test_admin_change_permission_pass():
-    requests.delete(f'{BASE_URL}/clear/v1')
-    user_info_reg_1 = {"email": "marryjoe@gmail.com", "password": "password", "name_first": "Marry", "name_last": "Joe"}
-    user_info_reg_2 = {"email": "marryjane@gmail.com", "password": "passwordJ", "name_first": "Marry", "name_last": "Jane"}
+def test_admin_change_permission_pass(setup):
+    marryjoe_data, marryjane_data, _ = setup
 
-    response_data_1 = requests.post(f'{BASE_URL}/auth/register/v2', json = user_info_reg_1)
-    response_data_2 = requests.post(f'{BASE_URL}/auth/register/v2', json = user_info_reg_2)
-
-    response_data_1 = response_data_1.json()
-    response_data_2 = response_data_2.json()
-
-    permission_change_data_1 = {'token': response_data_1['token'], 'u_id': response_data_2['auth_user_id'], 'permission_id': 1}
+    # Promote Marry Jane
+    permission_change_data_1 = {'token': marryjoe_data['token'], 'u_id': marryjane_data['auth_user_id'], 'permission_id': 1}
 
     requests.post(f'{BASE_URL}/admin/userpermission/change/v1', json = permission_change_data_1)
 
-def test_kick_pass():
-    requests.delete(f'{BASE_URL}/clear/v1')
-    user_info_reg_1 = {"email": "marryjoe@gmail.com", "password": "password", "name_first": "Marry", "name_last": "Joe"}
-    user_info_reg_2 = {"email": "marryjane@gmail.com", "password": "passwordJ", "name_first": "Marry", "name_last": "Jane"}
+def test_kick_pass(setup):
+    marryjoe_data, marryjane_data, _ = setup
 
-    response_data_1 = requests.post(f'{BASE_URL}/auth/register/v2', json = user_info_reg_1)
-    response_data_2 = requests.post(f'{BASE_URL}/auth/register/v2', json = user_info_reg_2)
-
-    response_data_1 = response_data_1.json()
-    response_data_2 = response_data_2.json()
-
-    kick_data = {'token': response_data_1['token'], 'u_id': response_data_2['auth_user_id']}
+    # Kick Marry Jane
+    kick_data = {'token': marryjoe_data['token'], 'u_id': marryjane_data['auth_user_id']}
     requests.delete(f'{BASE_URL}/admin/user/remove/v1', json = kick_data)
 
     print(requests.delete(f'{BASE_URL}/admin/user/remove/v1', json = kick_data).json())
 
-def test_access_error1():
-    requests.delete(f'{BASE_URL}/clear/v1')
-    user_info_reg_1 = {"email": "marryjoe@gmail.com", "password": "password", "name_first": "Marry", "name_last": "Joe"}
-    user_info_reg_2 = {"email": "marryjane@gmail.com", "password": "passwordJ", "name_first": "Marry", "name_last": "Jane"}
+def test_access_error1(setup):
+    marryjoe_data, marryjane_data, _ = setup
 
-    response_data_1 = requests.post(f'{BASE_URL}/auth/register/v2', json = user_info_reg_1)
-    response_data_2 = requests.post(f'{BASE_URL}/auth/register/v2', json = user_info_reg_2)
+    kick_data = {'token': marryjane_data['token'], 'u_id': marryjoe_data['auth_user_id']}
 
-    response_data_1 = response_data_1.json()
-    response_data_2 = response_data_2.json()
-
-    kick_data = {'token': response_data_2['token'], 'u_id': response_data_1['auth_user_id']}
+    # Remove Marry Joe
     response = requests.delete(f'{BASE_URL}/admin/user/remove/v1', json = kick_data)
     response = response.json()
     print(response)
     assert response['code'] == 403
 
 
-def test_invalid_user():
-    requests.delete(f'{BASE_URL}/clear/v1')
-    user_info_reg_1 = {"email": "marryjoe@gmail.com", "password": "password", "name_first": "Marry", "name_last": "Joe"}
-    user_info_reg_2 = {"email": "marryjane@gmail.com", "password": "passwordJ", "name_first": "Marry", "name_last": "Jane"}
+def test_invalid_user(setup):
+    marryjoe_data, marryjane_data, _ = setup
 
-    response_data_1 = requests.post(f'{BASE_URL}/auth/register/v2', json = user_info_reg_1)
-    response_data_2 = requests.post(f'{BASE_URL}/auth/register/v2', json = user_info_reg_2)
+    kick_data = {'token': marryjoe_data['token'], 'u_id': marryjane_data['auth_user_id'] + 3}
 
-    response_data_1 = response_data_1.json()
-    response_data_2 = response_data_2.json()
-
-    kick_data = {'token': response_data_1['token'], 'u_id': response_data_2['auth_user_id'] + 3}
+    # Kick user with Invalid ID
     response = requests.delete(f'{BASE_URL}/admin/user/remove/v1', json = kick_data)
     response = response.json()
     print(response)
     assert response['code'] == 400
 
 
-def test_wrong_permission_ID():
-    requests.delete(f'{BASE_URL}/clear/v1')
-    user_info_reg_1 = {"email": "marryjoe@gmail.com", "password": "password", "name_first": "Marry", "name_last": "Joe"}
-    user_info_reg_2 = {"email": "marryjane@gmail.com", "password": "passwordJ", "name_first": "Marry", "name_last": "Jane"}
+def test_wrong_permission_ID(setup):
+    marryjoe_data, marryjane_data, _ = setup
 
-    response_data_1 = requests.post(f'{BASE_URL}/auth/register/v2', json = user_info_reg_1)
-    response_data_2 = requests.post(f'{BASE_URL}/auth/register/v2', json = user_info_reg_2)
+    permission_change_data_1 = {'token': marryjoe_data['token'], 'u_id': marryjane_data['auth_user_id'], 'permission_id': 3}
 
-    response_data_1 = response_data_1.json()
-    response_data_2 = response_data_2.json()
-
-    permission_change_data_1 = {'token': response_data_1['token'], 'u_id': response_data_2['auth_user_id'], 'permission_id': 3}
-
+    # Change Marry Jane's permission to an invalid one
     data = requests.post(f'{BASE_URL}/admin/userpermission/change/v1', json = permission_change_data_1)
     data = data.json()
     assert data['code'] == 400
 
-def test_non_existent_user_id():
-    requests.delete(f'{BASE_URL}/clear/v1')
-    user_info_reg_1 = {"email": "marryjoe@gmail.com", "password": "password", "name_first": "Marry", "name_last": "Joe"}
-    user_info_reg_2 = {"email": "marryjane@gmail.com", "password": "passwordJ", "name_first": "Marry", "name_last": "Jane"}
+def test_non_existent_user_id(setup):
+    marryjoe_data, marryjane_data, _ = setup
 
-    response_data_1 = requests.post(f'{BASE_URL}/auth/register/v2', json = user_info_reg_1)
-    response_data_2 = requests.post(f'{BASE_URL}/auth/register/v2', json = user_info_reg_2)
+    permission_change_data_1 = {'token': marryjoe_data['token'], 'u_id': 10, 'permission_id': 1}
 
-    response_data_1 = response_data_1.json()
-    response_data_2 = response_data_2.json()
-
-    permission_change_data_1 = {'token': response_data_1['token'], 'u_id': 10, 'permission_id': 1}
-
+    # Change permision of a non-existent user
     data = requests.post(f'{BASE_URL}/admin/userpermission/change/v1', json = permission_change_data_1)
     data = data.json()
     assert data['code'] == 400
 
-def test_uid_only_global_owner():
-    requests.delete(f'{BASE_URL}/clear/v1')
-    user_info_reg_1 = {"email": "marryjoe@gmail.com", "password": "password", "name_first": "Marry", "name_last": "Joe"}
-    user_info_reg_2 = {"email": "marryjane@gmail.com", "password": "passwordJ", "name_first": "Marry", "name_last": "Jane"}
+def test_uid_only_global_owner(setup):
+    marryjoe_data, marryjane_data, _ = setup
 
-    response_data_1 = requests.post(f'{BASE_URL}/auth/register/v2', json = user_info_reg_1)
-    response_data_2 = requests.post(f'{BASE_URL}/auth/register/v2', json = user_info_reg_2)
+    permission_change_data_1 = {'token': marryjane_data['token'], 'u_id': marryjoe_data['auth_user_id'], 'permission_id': 2}
 
-    response_data_1 = response_data_1.json()
-    response_data_2 = response_data_2.json()
-
-    permission_change_data_1 = {'token': response_data_2['token'], 'u_id': response_data_1['auth_user_id'], 'permission_id': 2}
-
+    # Attempt to change Permission of Marry Joe
     data = requests.post(f'{BASE_URL}/admin/userpermission/change/v1', json = permission_change_data_1)
     data = data.json()
     assert data['code'] == 400
 
-def test_auth_not_global_owner():
-    requests.delete(f'{BASE_URL}/clear/v1')
-    user_info_reg_1 = {"email": "marryjoe@gmail.com", "password": "password", "name_first": "Marry", "name_last": "Joe"}
-    user_info_reg_2 = {"email": "marryjane@gmail.com", "password": "passwordJ", "name_first": "Marry", "name_last": "Jane"}
-    user_info_reg_3 = {"email": "marry@gmail.com", "password": "passwordz", "name_first": "Marry", "name_last": "Tom"}
+def test_auth_not_global_owner(setup):
+    marryjoe_data, marryjane_data, marry_data = setup
 
-
-    response_data_1 = requests.post(f'{BASE_URL}/auth/register/v2', json = user_info_reg_1)
-    response_data_2 = requests.post(f'{BASE_URL}/auth/register/v2', json = user_info_reg_2)
-    response_data_3 = requests.post(f'{BASE_URL}/auth/register/v2', json = user_info_reg_3)
-
-    response_data_1 = response_data_1.json()
-    response_data_2 = response_data_2.json()
-    response_data_3 = response_data_3.json()
-
-    permission_change_data_1 = {'token': response_data_1['token'], 'u_id': response_data_2['auth_user_id'], 'permission_id': 1}
+    permission_change_data_1 = {'token': marryjoe_data['token'], 'u_id': marryjane_data['auth_user_id'], 'permission_id': 1}
     requests.post(f'{BASE_URL}/admin/userpermission/change/v1', json = permission_change_data_1)
 
-    permission_change_data_2 = {'token': response_data_3['token'], 'u_id': response_data_2['auth_user_id'], 'permission_id': 2}
+    # Marry Tries to demote a user but she is not global owner
+    permission_change_data_2 = {'token': marry_data['token'], 'u_id': marryjane_data['auth_user_id'], 'permission_id': 2}
     data = requests.post(f'{BASE_URL}/admin/userpermission/change/v1', json = permission_change_data_2).json()
 
     assert data['code'] == 403
 
-def test_messages():
-    requests.delete(f'{BASE_URL}/clear/v1')
-    user_info_reg_1 = {"email": "marryjoe@gmail.com", "password": "password", "name_first": "Marry", "name_last": "Joe"}
-    user_info_reg_2 = {"email": "marryjane@gmail.com", "password": "passwordJ", "name_first": "Marry", "name_last": "Jane"}
-
-    response_data_1 = requests.post(f'{BASE_URL}/auth/register/v2', json = user_info_reg_1)
-    response_data_2 = requests.post(f'{BASE_URL}/auth/register/v2', json = user_info_reg_2)
-
-    response_data_1 = response_data_1.json()
-    response_data_2 = response_data_2.json()
+def test_messages(setup):
+    marryjoe_data, marryjane_data, _ = setup
 
 
-    channel_create_reg = {'token': response_data_1['token'], 'name': 'domchannel', 'is_public': True}
+    # Create a channel called domchannel
+    channel_create_reg = {'token': marryjoe_data['token'], 'name': 'domchannel', 'is_public': True}
     channel_create_data = requests.post(f'{BASE_URL}/channels/create/v2', json = channel_create_reg)
     channel_create_data = channel_create_data.json()
 
-    channel_inv_reg = {'token': response_data_1['token'], 'channel_id': channel_create_data['channel_id'], 'u_id': response_data_2['auth_user_id']}
+    # Invite marry jane
+    channel_inv_reg = {'token': marryjoe_data['token'], 'channel_id': channel_create_data['channel_id'], 'u_id': marryjane_data['auth_user_id']}
     channel_inv_data = requests.post(f'{BASE_URL}/channel/invite/v2', json = channel_inv_reg)
     channel_inv_data = channel_inv_data.json()
 
-    
-    message_send_reg = {'token': response_data_2['token'], 'channel_id': channel_create_data['channel_id'], 'message': 'HelloWorld'}
+    # Marry jane sends message to channel
+    message_send_reg = {'token': marryjane_data['token'], 'channel_id': channel_create_data['channel_id'], 'message': 'HelloWorld'}
     message_send_data = requests.post(f'{BASE_URL}/message/send/v1', json = message_send_reg)
     message_send_data = message_send_data.json()
 
-    message_send_reg = {'token': response_data_1['token'], 'channel_id': channel_create_data['channel_id'], 'message': 'HelloWorld'}
+    # Marry Joe sends message to channel
+    message_send_reg = {'token': marryjoe_data['token'], 'channel_id': channel_create_data['channel_id'], 'message': 'HelloWorld'}
     message_send_data = requests.post(f'{BASE_URL}/message/send/v1', json = message_send_reg)
     message_send_data = message_send_data.json()
 
-    kick_data = {'token': response_data_1['token'], 'u_id': response_data_2['auth_user_id']}
+    # Marry Joe kicks out Marry Jane
+    kick_data = {'token': marryjoe_data['token'], 'u_id': marryjane_data['auth_user_id']}
     response = requests.delete(f'{BASE_URL}/admin/user/remove/v1', json = kick_data)
     response = response.json()
 
-def test_only_one_global_owner():
-    requests.delete(f'{BASE_URL}/clear/v1')
+def test_only_one_global_owner(setup):
+    marryjoe_data, _, _ = setup
 
-    user_info_reg_1 = {"email": "marryjoe@gmail.com", "password": "password", "name_first": "Marry", "name_last": "Joe"}
+    kick_data = {'token': marryjoe_data['token'], 'u_id': marryjoe_data['auth_user_id']}
 
-    response_data_1 = requests.post(f'{BASE_URL}/auth/register/v2', json = user_info_reg_1).json()
+    # Marry Joe tries to kick herself
 
-    kick_data = {'token': response_data_1['token'], 'u_id': response_data_1['auth_user_id']}
     response = requests.delete(f'{BASE_URL}/admin/user/remove/v1', json = kick_data)
     response = response.json()
     assert response['code'] == 400
